3f90cfc35cffa58d90fb2c37670a8ce0e86cc934
erzagashi11 <121411956+erzagashi11@users.noreply.github.com>
2024-05-21 17:56:56 +0200

    Delete ClientServerRSA directory

--- ClientServerRSA/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.clientserverrsa {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.clientserverrsa to javafx.fxml;
-    exports com.example.clientserverrsa;
-}
