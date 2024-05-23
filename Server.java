ae1fb899ccf1f65077f824479b1c017c4343f2f8
erzagashi11 <121411956+erzagashi11@users.noreply.github.com>
2024-05-23 05:46:06 +0200

    Update Server.java

--- Server.java
+++ Server.java
@@ -3,130 +3,175 @@ import java.net.*;
 import java.security.*;
 import javax.crypto.*;
 import java.util.Base64;
+import java.util.concurrent.ConcurrentHashMap;
 
-public class Client {
+public class Server {
+    private ServerSocket serverSocket; // ServerSocket për pranimin e lidhjeve
+    private KeyPair keyPair; // Çifti i çelësave RSA për serverin
+    private ConcurrentHashMap<Socket, PublicKey> clients; // Map për ruajtjen e çelësave publike të klientëve
 
-    private Socket socket; // Socket-i për lidhjen me serverin
-    private PublicKey serverPublicKey; // Çelësi publik i serverit
-    private PrivateKey privateKey; // Çelësi privat i klientit
-    private PublicKey publicKey; // Çelësi publik i klientit
-    private DataInputStream input; // Input stream për të marrë të dhëna nga serveri
-    private DataOutputStream output; // Output stream për të dërguar të dhëna te serveri
-
-
-    public Client(String serverAddress, int serverPort) {
+    // Konstruktori për klasën Server, inicializon ServerSocket-in, çelësat dhe mapën e klientëve
+    public Server(int port) {
         try {
-            // Lidhja me serverin
-            socket = new Socket(serverAddress, serverPort); // Krijimi i një socket-i për lidhjen me serverin
-            System.out.println("Connected to server. Exchanging public keys..."); // Njoftimi për lidhjen me serverin
-            // Shkëmbimi i çelësave publike
-            exchangePublicKeys();
+            serverSocket = new ServerSocket(port); // Fillon serverin në portin e specifikuar
+            System.out.println("Server started and listening for connections...");
+            RSAKeyGenerator keyGenerator = new RSAKeyGenerator(); // Krijon një instancë të RSAKeyGenerator
+            keyPair = new KeyPair(keyGenerator.getPublicKey(), keyGenerator.getPrivateKey()); // Merr çelësat nga RSAKeyGenerator
+            clients = new ConcurrentHashMap<>(); // Inicializon mapën për ruajtjen e çelësave publike të klientëve
 
-            // Inicializimi i input/output streams
-            input = new DataInputStream(socket.getInputStream());
-            output = new DataOutputStream(socket.getOutputStream());
+            new Thread(new ConsoleInputHandler()).start(); // Fillon një thread për të trajtuar inputin nga console-a
 
-
-            new Thread(new ServerListener()).start(); // Startimi i një thread-i për dëgjimin e mesazheve nga serveri
-
-            // Leximi i komandave nga përdoruesi dhe procesimi i tyre
-            BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in)); // Krijimi i një reader për input nga përdoruesi
-            String command; // Deklarimi i një variabël për të ruajtur komandën e përdoruesit
             while (true) {
-                System.out.print("Enter command: "); // Njoftimi për përdoruesin për të futur një komandë
-                command = consoleReader.readLine(); // Leximi i komandës nga përdoruesi
-                processCommand(command); // Metoda për procesimin e komandës
+                Socket socket = serverSocket.accept(); // Pranon një lidhje të re nga klienti
+                System.out.println("New client connected. Exchanging public keys...");
+
+                // Bën shkëmbimin e çelësave publike me klientin
+                exchangePublicKeys(socket);
+
+                // Fillon një thread të ri për të trajtuar komunikimin me klientin
+                new ClientHandler(socket).start();
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    // Metoda për shkëmbimin e çelësave publike
-    private void exchangePublicKeys() throws Exception {
-        RSAKeyGenerator keyGenerator = new RSAKeyGenerator(); // Krijimi i një objekti për gjenerimin e çelësave RSA
-        privateKey = keyGenerator.getPrivateKey(); // Merrja e çelësit privat të klientit
-        publicKey = keyGenerator.getPublicKey(); // Merrja e çelësit publik të klientit
+    // Gjeneron çiftin e çelësave RSA
+    private KeyPair generateKeyPair() throws NoSuchAlgorithmException {
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+        keyGen.initialize(2048);
+        return keyGen.generateKeyPair();
+    }
 
-        // Marrja e çelësit publik të serverit
-        ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
-        serverPublicKey = (PublicKey) in.readObject();
-
-        // Dërgimi i çelësit publik të klientit te serveri
+    // Bën shkëmbimin e çelësave publike me klientin
+    private void exchangePublicKeys(Socket socket) throws Exception {
+        // Dërgon çelësin publik të serverit te klienti
         ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-        out.writeObject(publicKey);
+        out.writeObject(keyPair.getPublic());
 
-        System.out.println("Public key exchange complete. You can now send secure messages."); }
+        // Merr çelësin publik të klientit
+        ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+        PublicKey clientPublicKey = (PublicKey) in.readObject();
 
-    // Metoda për procesimin e komandave
-    private void processCommand(String command) {
-        try {
-            if (command.equalsIgnoreCase("exit")) {
-                socket.close(); // Mbyllja e lidhjes me serverin
-                System.exit(0); // Dalja nga aplikacioni
-            } else if (command.equalsIgnoreCase("viewkeys")) {
-                System.out.println("Client Public Key: " + Base64.getEncoder().encodeToString(publicKey.getEncoded())); // Shfaqja e çelësit publik të klientit
-            } else if (command.startsWith("send ")) {
-                String message = command.substring(5); // Marrja e mesazhit nga komanda duke hequr "send "
-                sendMessage(message); // Dërgimi i mesazhit te serveri
-            } else if (command.equalsIgnoreCase("help")) {
-                displayHelp();
-            } else {
-                System.out.println("Unknown command. Type 'help' to see available commands."); // Njoftimi për komandë të panjohur
+        // Ruajt çelësin publik të klientit
+        clients.put(socket, clientPublicKey);
+
+        System.out.println("Public key exchange complete. Ready to receive encrypted messages from clients.");
+    }
+
+    // Klasë e brendshme për trajtimin e komunikimit me klientin
+    private class ClientHandler extends Thread {
+        private Socket socket;
+        private DataInputStream input;
+        private DataOutputStream output;
+
+        // Konstruktori inicializon input dhe output streams
+        public ClientHandler(Socket socket) {
+            this.socket = socket;
+            try {
+                input = new DataInputStream(socket.getInputStream());
+                output = new DataOutputStream(socket.getOutputStream());
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
-    }
 
-    // Metoda për dërgimin e mesazhit të enkriptuar
-    private void sendMessage(String message) throws Exception {
-        Cipher cipher = Cipher.getInstance("RSA"); // Inicializimi i një objekti për enkriptim/dekriptim me algoritmin RSA
-        cipher.init(Cipher.ENCRYPT_MODE, serverPublicKey); // Inicializimi  për enkriptim dhe çelësin publik të serverit
-        byte[] encryptedBytes = cipher.doFinal(message.getBytes());
-        String encryptedMessage = Base64.getEncoder().encodeToString(encryptedBytes); // Konvertimi i mesazhit të enkriptuar në string të koduar Base64
-        System.out.println("Encrypted message sent to server: " + encryptedMessage);
-        output.writeUTF(encryptedMessage); // Dërgimi i mesazhit të enkriptuar në server
-        output.flush(); // Dërgimi i të dhënave nga buffer-i
-    }
-
-    // Metoda për shfaqjen e ndihmës
-    private void displayHelp() {
-        System.out.println("Available commands:");
-        System.out.println("  exit       - Close the connection and exit the client.");
-        System.out.println("  viewkeys   - Display the client's public key.");
-        System.out.println("  send <msg> - Send an encrypted message to the server.");
-        System.out.println("  help       - Display this help message.");
-    }
-
-    // Thread-i për dëgjimin e mesazheve nga serveri
-    private class ServerListener implements Runnable {
         @Override
         public void run() {
             try {
+                // Dëgjon për mesazhe të ardhura nga klienti
                 while (true) {
-                    String encryptedMessage = input.readUTF(); // Leximi i mesazhit të enkriptuar nga serveri
-                    String decryptedMessage = decryptMessage(encryptedMessage); // Dekriptimi i mesazhit
-                    System.out.println("Received message from server: " + decryptedMessage); // Shfaqja e mesazhit të dekriptuar nga serveri
+                    String encryptedMessage = input.readUTF(); // Lexon mesazhin e enkriptuar nga klienti
+                    String decryptedMessage = decryptMessage(encryptedMessage); // Dekripton mesazhin
+                    System.out.println("Received message from client: " + decryptedMessage); // Printon mesazhin e dekriptuar
                 }
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
 
-        // Metoda për dekriptimin e mesazhit
+        // Dekripton një mesazh të enkriptuar duke përdorur çelësin privat të serverit
         private String decryptMessage(String encryptedMessage) throws Exception {
-            Cipher cipher = Cipher.getInstance("RSA"); // Inicializimi i një objekti për enkriptim/dekriptim me algoritmin RSA
-            cipher.init(Cipher.DECRYPT_MODE, privateKey); // Inicializimi  për dekriptim dhe çelësin privat të klientit
-            byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedMessage)); // Dekriptimi i mesazhit
-            return new String(decryptedBytes); // Kthimi i mesazhit të dekriptuar si string
+            Cipher cipher = Cipher.getInstance("RSA");
+            cipher.init(Cipher.DECRYPT_MODE, keyPair.getPrivate());
+            byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedMessage));
+            return new String(decryptedBytes);
+        }
+
+        // Enkripton dhe dërgon një mesazh te klienti
+        public void sendMessage(String message) throws Exception {
+            Cipher cipher = Cipher.getInstance("RSA");
+            cipher.init(Cipher.ENCRYPT_MODE, clients.get(socket)); // Enkripton mesazhin me çelësin publik të klientit
+            byte[] encryptedBytes = cipher.doFinal(message.getBytes());
+            output.writeUTF(Base64.getEncoder().encodeToString(encryptedBytes)); // Dërgon mesazhin e enkriptuar
+            output.flush();
+        }
+    }
+
+    // Dërgon një mesazh të transmetuar në të gjithë klientët e lidhur
+    public void sendBroadcastMessage(String message) {
+        for (Socket socket : clients.keySet()) {
+            try {
+                new ClientHandler(socket).sendMessage(message); // Dërgon mesazhin duke përdorur ClientHandler
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 
 
+
+
+                    // Klasë e brendshme për trajtimin e inputit nga console-a
+    private class ConsoleInputHandler implements Runnable {
+        @Override
+        public void run() {
+            BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+            String command;
+            try {
+                while (true) {
+                    System.out.print("Enter command: ");
+                    command = consoleReader.readLine(); // Lexon komandën nga console-a
+                    processCommand(command); // Proceson komandën e dhënë
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Proceson komandat e dhëna në console
+        private void processCommand(String command) {
+            try {
+                if (command.equalsIgnoreCase("exit")) {
+                    System.exit(0); // Mbyll serverin
+                } else if (command.equalsIgnoreCase("viewkeys")) {
+                    System.out.println("Server Public Key: " + Base64.getEncoder().encodeToString(keyPair.getPublic().getEncoded())); // Shfaq çelësin publik të serverit
+                } else if (command.startsWith("send ")) {
+
+                    String message = command.substring(5); // Ekstrakton mesazhin nga komanda
+                    sendBroadcastMessage(message); // Dërgon mesazhin në të gjithë klientët
+                } else if (command.equalsIgnoreCase("help")) {
+                    displayHelp(); // Shfaq mesazhin ndihmës
+                } else {
+                    System.out.println("Unknown command. Type 'help' to see available commands.");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Shfaq komandat e disponueshme
+        private void displayHelp() {
+            System.out.println("Available commands:");
+            System.out.println("  exit       - Close the server.");
+            System.out.println("  viewkeys   - Display the server's public key.");
+            System.out.println("  send <msg> - Send an encrypted broadcast message to all clients.");
+            System.out.println("  help       - Display this help message.");
+        }
+    }
+
     public static void main(String[] args) {
-        String serverAddress = "localhost"; //  adresa e serverit
-        int serverPort = 1234; //  numrin i portit të serverit
-        new Client(serverAddress, serverPort); // Krijimi i një instance të klientit me adresën dhe portin e dhënë
+        int port = 1234; // Ndrysho këtë në numrin e portit të dëshiruar
+        new Server(port); // Fillon serverin
     }
 }
